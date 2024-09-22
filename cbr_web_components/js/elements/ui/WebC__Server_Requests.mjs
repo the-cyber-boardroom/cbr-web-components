ffb24b57da80f716a68e941f4ad22c84bf715671
Dinis Cruz <dinis.cruz@owasp.org>
2024-09-22 12:34:55 +0100

    minor fix

--- cbr_web_components/js/elements/ui/WebC__Server_Requests.mjs
+++ cbr_web_components/js/elements/ui/WebC__Server_Requests.mjs
@@ -9,7 +9,7 @@ export default class WebC__Server_Requests extends WebC__API_To_Table {
     async invoke_api_path() {
         let response = await super.invoke_api_path()
         for (let value of response.rows) {                 // todo: find a better way to do this (since at the moment we are hardcoding in code the location and target link)
-            if (response.rows.length > 5) {
+            if (response.rows.length > 3) {
                 let column_id = 0
                 let request_id = value[column_id]
                 value[column_id] = `<a href="docs/dev/web-components/server-request?request_id=${request_id}">${request_id}</a>`
