2b1432d9f4648940d07523e476755a89b4091123
Dinis Cruz <dinis.cruz@owasp.org>
2024-09-30 22:48:56 +0100

    major improvements (and some refactoring) to WebC__API__Side_Menu

---
+++ cbr_web_components/js/elements/api/WebC__API__Side_Menu.mjs
@@ -0,0 +1,154 @@
+import A                          from "../../core/A.mjs";
+import Div                        from "../../core/Div.mjs";
+import WebC__API_Markdown_To_Html from "./WebC__API_Markdown_To_Html.mjs";
+
+export default class WebC__API__Side_Menu extends WebC__API_Markdown_To_Html {
+
+    static url__api__data_file     = '/markdown/static_content/data-file?path='
+    static data_file__default_menu = 'en/web-pages/dev/web-components/api/side-menu/side-menu-1.toml'
+
+  // base class methods overrides
+    async connectedCallback() {
+        super.connectedCallback();
+    }
+
+    async load_attributes() {
+        super.load_attributes();
+        this.data_file = this.getAttribute('data_file') || WebC__API__Side_Menu.data_file__default_menu ;
+    }
+
+    async setup() {
+        this.menu_data = null
+        await super.setup()
+        await this.load_menu_data()
+
+        await this.load_material_icons()
+    }
+
+  // class methods
+
+    async load_material_icons() {
+         this.css_load_result = await this.load_libraries__css.load_material_design()
+    }
+
+    async build() {
+        this.renderMenu();
+    }
+
+    static class__side_menu           = 'side_menu_section';
+    static class__side_menu_item      = 'side_menu_item';
+    static class__side_menu_link      = 'side_menu_link';
+    static class__side_menu_icon      = 'side_menu_icon';
+    static class__side_menu_text      = 'side_menu_text';
+
+    renderMenu() {
+        const currentPath   = window.location.pathname;
+        const div_side_menu = new Div({class: WebC__API__Side_Menu.class__side_menu})
+        for (const key in this.menu_data) {
+            const item = this.menu_data[key];
+            if (item.visibility !== true){
+                continue
+            }
+            let class_icon            = `mdi me-2 ${item.icon} ${WebC__API__Side_Menu.class__side_menu_icon}`
+            let class__side_menu_item = WebC__API__Side_Menu.class__side_menu_item
+            let class__side_menu_link = WebC__API__Side_Menu.class__side_menu_link
+            let class__side_menu_text = WebC__API__Side_Menu.class__side_menu_text
+
+            if (currentPath.endsWith(item.href)) {
+                class__side_menu_item += '_active'                  // todo: fix this '_active' hack to deal with current lack for support for nested css rules
+                class__side_menu_link += '_active'
+                class__side_menu_text += '_active'
+            }
+            const div_side_menu_item  = new Div({class: class__side_menu_item   })
+            const a_side_menu_link    = new A  ({class: class__side_menu_link   , attributes: {href: item.href}})
+            const i_side_menu_icon    = new Div({class: class_icon              })
+            const div_side_menu_text  = new Div({class: class__side_menu_text   , value: item.text })
+            a_side_menu_link  .add_elements(i_side_menu_icon, div_side_menu_text)
+            div_side_menu_item.add_element (a_side_menu_link                    )
+            div_side_menu     .add_element (div_side_menu_item                  )
+
+        }
+        this.set_inner_html(div_side_menu.html())
+    }
+
+    css_rules() {
+        return {
+            [`.${WebC__API__Side_Menu.class__side_menu_section}`]: {
+                'width': '220px', // Adjusted width
+                'padding': '15px 10px',
+                'background-color': 'red' ,
+                'border-right': '1px solid #ddd',
+                'height': '100vh',
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_item}`]: {
+                'display'        : 'flex',
+                'align-items'    : 'center',
+                'padding'        : '8px 0px 8px 15px',
+                'margin'         : '0px 15px',
+                'cursor'         : 'pointer',
+                'border-radius'  : '4px',
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_item}_active`]: {
+                'display'         : 'flex',
+                'align-items'     : 'center',
+                'padding'         : '4px 0px 4px 15px',
+                'margin'          : '0px 15px',
+                'cursor'          : 'pointer',
+                'border-radius'   : '5px',
+                'background-color': '#26c6da',
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_link}`]: {
+                'display'        : 'flex',
+                'align-items'    : 'center',
+                'text-decoration': 'none',
+                'color'          : '#96a5ac' ,
+                'width'          : '100%',
+                // '.active' : {                                            // todo: this need fix on Web_Component.create_stylesheet_from_css_rules()
+                //     'background-color': 'red'
+                // }
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_link}_active`]: {       // short term hack to deal with prob with nested css rules
+                'display'         : 'flex'   ,
+                'align-items'     : 'center' ,
+                'text-decoration' : 'none'   ,
+                'color'           : '#FFFFFF',
+                'width'           : '100%'   ,
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_link}:hover`]: {
+                'color': '#26c6da',
+            },
+
+            [`.${WebC__API__Side_Menu.class__side_menu_icon}`]: {
+                'font-size'   : '20px', // Adjust icon size
+                'margin-right': '12px',
+            },
+
+            [`.${WebC__API__Side_Menu.class__side_menu_text}`]: {
+                'font-size'      : '15px',
+                'font-weight'    : '100',
+                'color'          : '#54667a',
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_text}_active`]: {
+                'font-size'      : '15px',
+                'font-weight'    : '400',
+                'color'          : '#FFFFFF',
+            },
+            [`.${WebC__API__Side_Menu.class__side_menu_text}:hover`]: {
+                'color'         : '#26c6da',
+            },
+        };
+    }
+
+
+    //todo: replace this with dynamic data
+
+    async load_menu_data() {
+        const url_data = WebC__API__Side_Menu.url__api__data_file + this.data_file
+        console.log(url_data)
+        const method   = 'GET'
+        this.menu_data = await this.api_invoke.invoke_api(url_data, method)
+        console.log(this.menu_data)
+    }
+}
+
+WebC__API__Side_Menu.define()
---
+++ cbr_web_components/qunit/elements/api/WebC__API__Side_Menu.qunit.js
@@ -0,0 +1,85 @@
+import WebC__Target_Div             from "../../../js/utils/WebC__Target_Div.mjs";
+import Web_Component                from "../../../js/core/Web_Component.mjs";
+import API__Invoke                  from "../../../js/data/API__Invoke.mjs";
+import WebC__API__Side_Menu         from "../../../js/elements/api/WebC__API__Side_Menu.mjs";
+import WebC__API_Markdown_To_Html   from "../../../js/elements/api/WebC__API_Markdown_To_Html.mjs";
+import Load_Libraries__CSS          from "../../../js/utils/Load_Libraries__CSS.mjs";
+
+
+
+QUnit.module('WebC__API__Side_Menu', function(hooks) {
+    let target_div
+    let webc__api_side_menu
+    let mock_responses
+    let api_path
+    let data_file
+
+    hooks.beforeEach(async (assert) =>{
+        data_file                = WebC__API__Side_Menu.data_file__default_menu
+        target_div               = WebC__Target_Div.add_to_body()
+        mock_responses           = JSON.stringify(api_mock_data())
+        let attributes           = { data_file: data_file, mock_responses: mock_responses, api_path: api_path }
+        webc__api_side_menu = await target_div.append_child(WebC__API__Side_Menu, attributes)
+    })
+
+    hooks.afterEach(() => {
+        webc__api_side_menu.remove()
+        target_div              .remove()
+    })
+
+    function api_mock_data() {
+        const url__css__material_design_icons              = Load_Libraries__CSS.url__css__material_design_icons
+
+        const  url__api__data_file = WebC__API__Side_Menu.url__api__data_file + WebC__API__Side_Menu.data_file__default_menu
+        return { [url__api__data_file            ] : expected_menu_data      ,
+                 [url__css__material_design_icons] : '.simple {css : "code"} ' }
+    }
+
+    QUnit.test('.constructor', (assert) => {
+        assert.deepEqual(target_div.constructor.name                  , 'WebC__Target_Div'                            )
+        assert.deepEqual(WebC__API__Side_Menu.name                    , 'WebC__API__Side_Menu'                   )
+        assert.deepEqual(webc__api_side_menu.data_file                , data_file                                  )
+        assert.deepEqual(webc__api_side_menu.getAttributeNames()      , ['data_file', 'mock_responses', 'api_path'])
+        assert.deepEqual(webc__api_side_menu.api_invoke.mock_responses, JSON.parse(mock_responses)                    )
+
+        assert.ok       (WebC__API__Side_Menu.prototype          instanceof Web_Component              )
+        assert.ok       (webc__api_side_menu                instanceof WebC__API_Markdown_To_Html )
+        assert.ok       (webc__api_side_menu                instanceof Web_Component              )
+        assert.ok       (webc__api_side_menu                instanceof HTMLElement                )
+        assert.ok       (webc__api_side_menu.api_invoke     instanceof API__Invoke                )
+        assert.ok       (webc__api_side_menu.css_load_result.css_loaded)
+    })
+
+    QUnit.test('.build', (assert) => {
+        assert.deepEqual(webc__api_side_menu.inner_html(), expected_html    )
+    })
+
+    QUnit.test('.load_menu_data', (assert) => {
+        assert.deepEqual(webc__api_side_menu.menu_data, expected_menu_data)
+    })
+
+    const expected_menu_data = {
+          'home'  : { 'href': 'home'  , 'icon': 'mdi-home' , 'text': 'Home'  , 'logged_in': false , 'visibility': true },
+          'athena': { 'href': 'athena', 'icon': 'mdi-robot', 'text': 'Athena', 'logged_in': false , 'visibility': true },
+      };
+    const expected_html = `\
+<div class="side_menu_section">
+    <div class="side_menu_item">
+        <a class="side_menu_link" href="${expected_menu_data.home.href}">
+            <div class="mdi me-2 ${expected_menu_data.home.icon} side_menu_icon">
+            </div>
+            <div class="side_menu_text">${expected_menu_data.home.text}</div>
+        </a>
+    </div>
+    <div class="side_menu_item">
+        <a class="side_menu_link" href="${expected_menu_data.athena.href}">
+            <div class="mdi me-2 ${expected_menu_data.athena.icon} side_menu_icon">
+            </div>
+            <div class="side_menu_text">${expected_menu_data.athena.text}</div>
+        </a>
+    </div>
+</div>
+`
+
+
+})
